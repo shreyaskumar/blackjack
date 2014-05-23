f169124c416ffcd82282843ea33443df3131a10d
Shreyas Kumar <shreyas.kumar@gmail.com>
2014-05-22 23:33:40 -0700

    commented version

--- blackjackClasses.rb
+++ blackjackClasses.rb
@@ -1,26 +1,37 @@
 class Player
+	#the basic blackjack player
 	def initialize(name)
+		#initializes a player with a name
+		#other variables to be instantiated include the list of Hands (usually just 1, can be more than 1 for a split)
+		#numHands: the number of hands
+		#list of bets that correspond to the hands. Note that bets can be different on split hands since you can double 
+		#down after splitting
+		#pot represents the total money the player has
+		#finally an ace flag to facilitate scoring
 		@name = name
 		@handList = Array.new(1){Array.new}
 
 		@numHands = 1
 		@bet = [0]
 		@pot = 1000
-		@hasAce = false #flag to account for two possible scorings with aces
+		@hasAce = false 
 	end
     
     def getName()
+    	#returns the player's name: primarily for the UI output
     	return @name
     end
 
 	def addCard(card, numHand = 0)
+		#adds a card to the specified hand
 		@handList[numHand].push(card)
 		if card.getName() == 'A'
-			@hasAce = true
+			@hasAce = true #set the ace flag to true when required
 		end
 	end
 
 	def printHand(numHand = 0)
+		#prints the specified hand out in a nice format
 		puts "#{@name}'s hand (number #{numHand + 1}) is"
 		for i in 0..@handList[numHand].count-1
 			puts "#{@handList[numHand][i].getName()} of #{@handList[numHand][i].getSuit()}"
@@ -28,6 +39,7 @@ class Player
 	end
 
 	def addHand(card)
+		#adds a hand to the player. Used only when split.
 		@handList.push(Array.new)
 		self.addCard(card, @numHands)
 		@numHands += 1
@@ -35,52 +47,58 @@ class Player
 	end
 		
 	def popCard(numHand)
-		@handList[numHand].pop #to be called only when splitting hands to change the original hand to one card
+		#to be called only when splitting hands to change the original hand to one card
+		@handList[numHand].pop 
 	end
 
 	def getNumHands()
+		#returns the number of hands
 		return @numHands
 	end
 
-	def setNumHands(num)
-		@numHands = num
-	end
-
 	def getHand(num = 0)
+		#returns the specified hand
 		return @handList[num]
 	end
 
 	def getBet(numHand = 0)
+		#returns the bet associated with the specified hand
 		return @bet[numHand]
 	end
 
 	def setBet(numHand = 0, num)
+		#sets the bet of numHand hand to num
 		@bet[numHand] = num
 	end
 
 	def totalBet()
+		#returns the total outstanding bet over all hands
 		return @bet.inject{|sum, x| sum + x}
 	end
 
 	def getPot()
+		#returns total amount of money available, or the pot
 		return @pot
 	end
 
 	def setPot(amount)
+		#sets the pot (total money available)
 		@pot = amount
 	end
    
 
     def getValue(num = 0)
+    	#returns the value of the specifed (through num) hand
     	value = 0
     	if @hasAce == true
-    		altValue = 0
+    		altValue = 0 #alternate scoring with an ace
     	end
     	for i in @handList[num]
     		if i.getName() == 'A'
-    			value += 11
+    			value += 11 #default score of 11
     			altValue += 1
     		else
+    			#scoring for all other cards
     			value += i.getCardValue()
     			if @hasAce == true
     				altValue += i.getCardValue()
@@ -88,18 +106,14 @@ class Player
     		end
     	end
     	if value > 21 && @hasAce == true
-    		return [altValue]
+    		return [altValue] #if scoring an ace as 11 results in a bust, only score as a 1
     	else
-    		return [value, altValue]
+    		return [value, altValue] #return both values if no bust occurs, the choice is upto the user
     	end
     end
 
-    def existAce()
-    	return @hasAce
-    end
-
-
     def reset()
+    	#at the end of a round, resets all initial parameters to their defaults
     	@numHands = 1
     	@handList = Array.new(1){Array.new}
     	@bet = [0]
@@ -108,11 +122,14 @@ class Player
 end
 
 class Dealer < Player
+	#dealer class that is an extension of the player class
 	def showCard()
+		#method to show the second card of the dealer
 		puts "Dealer shows #{@handList[0][1].getName()} of #{@handList[0][1].getSuit()}"
 	end
 
 	def printHand(numHand = 0)
+		#change the printHand method to avoid printing the hand number, since the dealer can never split
 		puts "#{@name}'s hand is"
 		for i in 0..@handList[numHand].count-1
 			puts "#{@handList[numHand][i].getName()} of #{@handList[numHand][i].getSuit()}"
@@ -122,7 +139,11 @@ end
 
 
 class Card
+	#card class that is used to build the deck
 	def initialize(suit,name)
+		#three instantiation parameters:
+		#the name, suit and blackjack value. 
+		#Number cards have the same value as the number, face cards are worth 10 and ace is 11 or 1.
 		@suit = suit
 		@name = name
 		if name.class == Fixnum
@@ -135,14 +156,17 @@ class Card
 	end
 
 	def getName()
+		#returns the name of the card
 		return @name
 	end
 
 	def getCardValue()
+		#returns the value of the card
 		return @value
 	end
 
 	def getSuit()
+		#returns the suit of the card
 		return @suit
 	end
 end
--- gamePlay.rb
+++ gamePlay.rb
@@ -65,15 +65,16 @@ def initialDeal(deck, playerList)
 end
 
 def placeBets(playerList)
-	for i in 0..playerList.count - 2
-		while true
+	#method that allows each player to place bets. Bets can only be integers, and have to be less than their pot.
+	for i in 0..playerList.count - 2 #cycle through all players except for the dealer (dealer is the last element of playerList)
+		while true 
 			puts "#{playerList[i].getName()}, place your bet. You're pot is #{playerList[i].getPot()}."
 			bet = gets
 			if !(bet.chomp.class == Fixnum)
 				"This game only accepts integer bets"
 			end
 			if bet.chomp.to_i < playerList[i].getPot()
-				playerList[i].setBet(0, bet.chomp.to_i)
+				playerList[i].setBet(0, bet.chomp.to_i) #sets the bet as specified
 				break
 			else
 				puts "You're pot is not big enough to support that bet."
@@ -84,9 +85,10 @@ def placeBets(playerList)
 end
 
 def hit(player, deck, numHand = 0)
+	#method that allows a player/dealer to hit
 	cardToDeal = dealCard(deck)
-	player.addCard(cardToDeal, numHand)
-	deck.delete(cardToDeal)
+	player.addCard(cardToDeal, numHand) #add card to the hand
+	deck.delete(cardToDeal) #delete the drawn card from the deck
 	puts "#{player.getName()} has been dealt #{cardToDeal.getName()} of #{cardToDeal.getSuit()}"
 	return deck
 end
@@ -103,23 +105,27 @@ def playSingleTurn(player, deck, numHand = 0)
 	#plays the turn of one player
 	puts "#{player.getName()}'s turn, hand #{numHand}."
 
+	#in case this is a split hand with just one card, draw the second card
 	hand = player.getHand(numHand)
 	if hand.count == 1
 		deck = hit(player, deck, numHand)
 	end
 
+	#print out the hand values and the handfor player convenience
 	if player.getValue()[1] != nil
 		puts "#{player.getName()} is at #{player.getValue()[0]} with A at 11 or at #{player.getValue()[1]} with A at 1"
 	else
 		puts "#{player.getName()} is at #{player.getValue()[0]}"
 	end
 	player.printHand(numHand)
-		
+	
+	#check for blackjack on initial deal
 	if player.getValue()[0] == 21
 		puts "You have hit blackjack! Now wait for the dealer's turn to decide winnings."
 		return [deck, player]
 	end
 
+	#ask the user if he/she wants to split. Checks to make sure both cards are the same, there are only two cards, and the player can afford the second bet in the split
 	if hand.count == 2 && hand[0].getCardValue() == hand[1].getCardValue() && ((player.totalBet() + player.getBet(numHand)) < player.getPot()) 
 		puts "Do you want to split? y/n"
 		answerSplit = gets
@@ -129,16 +135,18 @@ def playSingleTurn(player, deck, numHand = 0)
 		end
 	end
 
+	#check if the user wants to double down. Again, only possible if player has enough money
 	if (player.totalBet() + player.getBet(numHand)) < player.getPot()
 		puts "Do you wish to double down? y/n"
 		answer = gets
 		if answer.chomp == 'y'
-			player.setBet(2*player.getBet(numHand))
+			player.setBet(2*player.getBet(numHand)) #change the bet to double the initial
 		end
 	end
 	flag = false #to print the hand in the while loop
 	while true
 		if flag
+			#prints out the hand value and hand for user convenience as the user hits
 			if player.getValue()[1] != nil
 				puts "#{player.getName()} is at #{player.getValue()[0]} with A at 11 or at #{player.getValue()[1]} with A at 1"
 			else
@@ -149,22 +157,27 @@ def playSingleTurn(player, deck, numHand = 0)
 		flag = true
 		puts "Do you want to hit or stay? h/s"
 		answer = gets
+		#is user hits
 		if answer.chomp == 'h'
 			deck = hit(player, deck, numHand)
+			#if the player busts, change the pot, reset the bet
 			if player.getValue(numHand)[0] > 21
 				puts "#{player.getName()} has busted with hand #{numHand + 1}. You lose your bet of #{player.getBet()}."
 				player.setPot(player.getPot() - player.getBet(numHand))
 				player.setBet(numHand, 0)
 				puts "Your pot is now #{player.getPot()}."
+				#check if the player has lost all his/her money. 
 				if player.getPot() == 0
 					puts "You have no money left, you have lost."
 					break
 				end
 				break
+			#player at blackjack
 			elsif player.getValue(numHand)[0] == 21
 				puts "You have hit blackjack! Now wait for the dealer's turn to decide winnings."
 				break
 			end
+		#player stays
 		elsif answer.chomp == 's'
 			puts "You chose to stay. Your hand's value is #{player.getValue()[0]}. Now wait for the dealer's turn to decide winnings."
 			break
@@ -176,17 +189,22 @@ def playSingleTurn(player, deck, numHand = 0)
 end
 
 def playTurnDealer(dealer, deck)
+	#dealer plays his turn. Counts an ace as a 11 unless it means he busts. Always hits at 16 or less, and stays at 17 or more
 	while true
 		puts "#{dealer.getName()} is at #{dealer.getValue()[0]}"
 		dealer.printHand()
 		dealerValue = dealer.getValue()[0]
+		#if dealer busts
 		if dealerValue > 21
 			puts "Dealer has busted. Any players who did not bust will win."
 			break
+		#if dealer needs to hit again
 		elsif dealerValue <= 16
 			returnStuff = hit(dealer, deck)
 			deck = returnStuff
-	    else 
+		#if dealer is at blackjack or stays
+	    else
+	    #nested if to facilitate break 
 	    	if dealerValue == 21
 	    		puts "Dealer hits blackjack!"
 	    	else
@@ -202,15 +220,16 @@ end
 
 def playEntireTurn(playerList, deck)
 	#plays the turn for all players including dealer.
+	#cycle through players, and all hands of each player
 	for i in 0..playerList.count-1
 		numHand = 0
 		while numHand <  playerList[i].getNumHands()
 			if i != playerList.count-1
-				returnStuff = playSingleTurn(playerList[i], deck, numHand)
+				returnStuff = playSingleTurn(playerList[i], deck, numHand) #play the turn
 				deck = returnStuff[0]
 				playerList[i] = returnStuff[1]
-				if !(returnStuff[2] == true) #if the turn did not result in a split
-					numHand += 1
+				if !(returnStuff[2] == true) #if the turn did not result in a split, go to next hand
+					numHand += 1             #else, stay on the same hand
 				end
 			else
 				returnStuff = playTurnDealer(playerList[i], deck) #dealer plays turn according to the rules
@@ -221,28 +240,35 @@ def playEntireTurn(playerList, deck)
 		end
 	end
     
+    #scoring to see who wins, for those who did not bust
     dealerValue = playerList.last.getValue()[0]
     j = 0
+    #iterate through players and each hand
     while j < playerList.count - 1
         currPlayer = playerList[j]
         for numHand in 0..currPlayer.getNumHands() - 1
 	    	currValue = currPlayer.getValue(numHand)[0]
+	    	#if the player wins (closer to 21 (but less than or equal to it) than the dealer)
 	    	if ((currValue > dealerValue) && (currValue <= 21)) || ((dealerValue > 21) && (currValue <= 21))
 	    		currPlayer.setPot(currPlayer.getPot() + (2*currPlayer.getBet(numHand)/3).to_i )
 	    		currPlayer.setBet(numHand, 0)
 	    		puts "#{currPlayer.getName()}'s value of #{currValue} in hand #{numHand+1} beats the dealer. Your pot is #{currPlayer.getPot()}"
+	    	#if the player loses (opposite of above)
 	    	elsif (currValue < dealerValue) && (currValue <= 21)
 	    		currPlayer.setPot(currPlayer.getPot() - currPlayer.getBet(numHand))
 	    		currPlayer.setBet(numHand, 0)
 	    		puts "#{currPlayer.getName()}'s value of #{currValue} in hand #{numHand+1} loses to the dealer. Your pot is #{currPlayer.getPot()}"
+	    		#check to see if the player is bankrupt
 	    		if currPlayer.getPot() == 0
 					puts "You have no money left, you have lost."
 				end
+			#cgeck for a tieS
 	    	elsif (currValue == 21 && dealerValue == 21)
 	    		currPlayer.setBet(numHand, 0)
 	    		puts "We have a tie. You neither win nor lose; you get your money back."
 	    	end
 	    end
+	    #if a player is bankrupt, remove them from the table
 	    if currPlayer.getPot() == 0
 	    	playerList.delete_at(j)
 	    else
@@ -253,7 +279,7 @@ def playEntireTurn(playerList, deck)
 end
 
 
-#main script starts here. class definitions in LiveRamp.rb, other functions that are called are defined above
+#script starts here. class definitions in LiveRamp.rb, other functions that are called are defined above
 deck = buildDeck()
 #create dealer
 dealer = Dealer.new('Dealer')
@@ -265,26 +291,31 @@ playerList.push(dealer) #the last player is the dealer (gets dealt cards last)
 while playerList.count > 1
 	#deal cards and update the player list and deck
 	playerList = placeBets(playerList)
-	returnDeal = initialDeal(deck, playerList)
+	returnDeal = initialDeal(deck, playerList) #necessary because ruby does not use pass by reference
 	deck = returnDeal[0]
 	playerList = returnDeal[1]
+	#dealer shows his second card
 	playerList.last.showCard()
+	#all players + dealer play
 	returnDeal = playEntireTurn(playerList, deck)
 	deck = returnDeal[0]
 	playerList = returnDeal[1]
 
+	#option to opt out
 	puts "Do you want to stop playing?"
 	answer = gets
 	if answer.chomp == 'y'
 		break
 	else
 		for i in playerList
-			i.reset()
+			i.reset() 			#continue playing
 			deck = buildDeck()
 		end
 	end
 
 end
+
+#ending messages
 if playerList.count  == 1
 	puts "All players have lost."
 end
